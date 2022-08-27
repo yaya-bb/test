1c4b295b3daaaf580982c1a3dbf6932000beced9
yaya-bb <2286834433@qq.com>
2022-08-27 22:31:31 +0800

    修改

--- BaiDu/baidutest/src/components/NavHeader.vue
+++ BaiDu/baidutest/src/components/NavHeader.vue
@@ -9,13 +9,57 @@
       <a href="https://image.baidu.com/" target="_blank" class="link">图片</a>
       <a href="https://pan.baidu.com/disk/main?from=1026962h#/index?category=all" target="_blank" class="link">网盘</a>
       <a href="https://www.baidu.com/more/" target="_blank" class="link">更多</a>
-
+    </div>
+    <div class="right">
+      <!-- 天气插件 -->
+      <!-- <div id="he-plugin-simple" class="weather"></div> -->
+      <div class="settings">
+        <span>设置</span>
+        <div class="children">
+          <ul>
+            <li class="setting">搜索设置</li>
+            <li class="setting">高级搜索</li>
+            <li class="setting">关闭预测</li>
+            <li class="setting">隐私设置</li>
+            <li class="setting">隐藏资讯</li>
+            <li class="setting">关闭播报</li>
+            <li class="setting">更换皮肤</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'NavHeader'
+  // name: 'NavHeader',
+  // mounted() {
+  //   window.WIDGET = {
+  //       "CONFIG": {
+  //         "modules": "1204",
+  //         "background": "5",
+  //         "tmpColor": "222222",
+  //         "tmpSize": "13",
+  //         "cityColor": "222222",
+  //         "citySize": "13",
+  //         "aqiColor": "222222",
+  //         "aqiSize": "13",
+  //         "weatherIconSize": "23",
+  //         "alertIconSize": "18",
+  //         "padding": "10px 10px 10px 10px",
+  //         "shadow": "0",
+  //         "language": "auto",
+  //         "fixed": "false",
+  //         "vertical": "top",
+  //         "horizontal": "left",
+  //         "key": "e95f6ead53c143e4ad359e2c76cc0825"
+  //       }
+  //     }
+  //     let script = document.createElement('script');
+  //     script.type = 'text/javascript';
+  //     script.src = 'src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0';
+  //     document.getElementsByName('head')[0].appendChild(script);
+  // }
 }
 </script>
 <style lang="scss" scoped>
@@ -27,12 +71,35 @@ export default {
     height: 60px;
     padding-left: 24px;
     .link {
-      color: #222;
+      color: blank;
       display: inline-block;
       position: relative;
       margin-top: 24px;
       margin-right: 19px;
       font-size: 13px;
+    }
+  }
+  .right {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding-left: 200px;
+    padding-right: 24px;
+    .settings {
+      position: relative;
+      display: inline-block;
+      vertical-align: top;
+      margin-top: 19px;
+      span {
+        cursor: pointer;
+      }
+      &:hover {
+        .children {
+          width: 80px;
+          height: 210px;
+          opacity: 1;
+        }
+      }
     }
   }
 </style>
